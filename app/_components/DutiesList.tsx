0b2f344deda6c8e2796c83b2920c0050b4d59393
ito-ito <ittooco@gmail.com>
2024-05-10 08:40:00 +0900

    :bug: Backendに通信できない不具合の修正

--- app/_components/DutiesList.tsx
+++ app/_components/DutiesList.tsx
@@ -1,6 +1,5 @@
 const fetchDuties = async () => {
-  const response = await fetch("http://172.18.0.1:3000/duties", {
-    // const response = await fetch("http://back:3000/duties", {
+  const response = await fetch("http://back:3000/duties", {
     cache: "no-store",
   });
   return response.json();
@@ -12,7 +11,7 @@ export const DutiesList = async () => {
   return (
     <>
       {duties.map((duty) => (
-        <p>{duty.title}</p>
+        <p key={duty.id}>{duty.title}</p>
       ))}
     </>
   );
