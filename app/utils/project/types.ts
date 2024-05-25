77b4b20685a5a8b277c91fcd89c57ede856dca77
ito-ito <ittooco@gmail.com>
2024-05-25 09:20:43 +0900

    :sparkles: Projectに詳細を表示

--- app/utils/project/types.ts
+++ app/utils/project/types.ts
@@ -1,6 +1,7 @@
 export interface Project {
   id: number;
   title: string;
+  description: string;
   start_date: string;
   end_date: string;
   created_at: string;
--- features/components/Project/ProjectList.tsx
+++ features/components/Project/ProjectList.tsx
@@ -30,22 +30,35 @@ const ProjectList = (props: Props) => {
       </h2>
       <Carousel className="mx-8">
         <CarouselContent>
-          {projects.map((project) => (
-            <CarouselItem key={project.id} className="md:basis-1/2 lg:basis-1/3">
-              <Card className="h-full">
-                <CardHeader>
-                  <CardTitle>{project.title}</CardTitle>
-                  <CardDescription>description</CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <p>Content</p>
-                </CardContent>
-                <CardFooter>
-                  <p>Footer</p>
-                </CardFooter>
-              </Card>
-            </CarouselItem>
-          ))}
+          {projects.map((project) => {
+            const options = {
+              year: "numeric",
+              month: "short",
+            };
+            const startDate = new Intl.DateTimeFormat("en-US", options).format(
+              new Date(project.start_date)
+            );
+            const endDate =
+              project.end_date &&
+              Intl.DateTimeFormat("en-US", options).format(new Date(project.end_date));
+
+            return (
+              <>
+                <CarouselItem key={project.id} className="md:basis-1/2 lg:basis-1/3">
+                  <Card className="h-full">
+                    <CardHeader>
+                      <CardTitle>{project.title}</CardTitle>
+                      <CardDescription>
+                        {startDate} -{endDate && `${endDate}`}
+                      </CardDescription>
+                    </CardHeader>
+                    <CardContent>{project.description}</CardContent>
+                    <CardFooter></CardFooter>
+                  </Card>
+                </CarouselItem>
+              </>
+            );
+          })}
           <CarouselItem className="md:basis-1/2 lg:basis-1/3">
             <Card className="h-full content-center text-center">
               <Button variant="link">Show more...</Button>
